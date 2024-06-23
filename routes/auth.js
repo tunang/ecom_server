b59101eed53b351d468f54a31bf6c724b2047630
tunang <nguyentuan20042207@gmail.com>
2024-06-23 21:57:09 +0700

    new

--- routes/auth.js
+++ routes/auth.js
@@ -201,7 +201,7 @@ router.delete('/logout', verifyToken, async (req, res) => {
 const generateTokens = (payload) => {
   //create jwt
   const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15s",
+    expiresIn: "60s",
   });
 
   const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {
